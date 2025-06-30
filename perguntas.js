8abcfc96fbec95e0bb0ccc94ef417338bb4482d9
naoeajuliaaaa <julia.lima.rodrigues@escola.pr.gov.br>
2025-06-30 11:29:23 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
+    'h',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quais são os oceanos existentes?',
+    'os oceanos são Pacífico,Atlântico,Índico,Ártico e Antártico'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matemática',
+    'Quanto é 3² + 5?',
+    '3² + 5 é 14'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz gato em Inglês?',
+    'gato em ingles é cat (KET)'
 )
